693288358222f1e1c0476eb0148afb5abf20ec40
SafiMoustapha <157801425+SafiMoustapha@users.noreply.github.com>
2025-04-14 22:19:28 +0200

    ajout

--- script.js
+++ script.js
@@ -1,30 +1,36 @@
 let isClicked = false;
+const img = document.getElementById('menuImage');
+const hoverImg = document.getElementById('hoverImage');
+const menu = document.getElementById('sideMenu');
+const menuToggle = document.getElementById('menuToggle');
 
 function showHoverImage() {
     if (!isClicked) {
-        document.getElementById('menuImage').classList.add('opacity-0');
-        document.getElementById('hoverImage').classList.remove('opacity-0');
+        img.src = './images/menu (3).png';
+        img.style.width = '1.25rem';
     }
 }
 
 function resetImage() {
     if (!isClicked) {
-        document.getElementById('menuImage').classList.remove('opacity-0');
-        document.getElementById('hoverImage').classList.add('opacity-0');
+        document.getElementById('menuImage').src = './images/menu (4).png';
     }
 }
 
 function changeImage() {
-    const img = document.getElementById('menuImage');
-    const hoverImg = document.getElementById('hoverImage');
-
+    
     if (!isClicked) {
-        img.src = './images/croix.png'; // Image après clic
-        hoverImg.classList.add('hidden'); // Masquer l’image de survol après clic
+        img.src = './images/croix.png';
+        img.style.borderRadius = '10%';
+        menuToggle.style.border = '1px solid #497b9c';
+        menuToggle.style.borderRadius = '10%';
+        menu.style.display = 'block';
+        menu.classList.add('slide-out');
         isClicked = true;
     } else {
-        img.src = './images/menu (4).png'; // Retour à l’image initiale
-        hoverImg.classList.remove('hidden');
+        img.src = './images/menu (4).png';
+        menuToggle.style.border = 'none'; // <- Pour retirer la bordure à la fermeture
+        menu.style.display = 'none';
         isClicked = false;
     }
 }
